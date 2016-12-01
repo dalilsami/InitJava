449599a43dddb32d6a9027aec4481571540e9ec5
dalil_s <dalil_s@d52c74ed-5b9c-4f4e-8b18-0e6fbfed2a89>
2016-12-01 13:58:28 +0000

    display form inactif si forbidden

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 function display_form() {
-    if (document.getElementById("form-container") === null) {
-        var container = document.createElement("SECTION");
-        var form = document.createElement("FORM");
-        var title = document.createElement("SECTION");
-        var input = document.createElement("SECTION");
-        var login = document.createElement("INPUT");
-        var password = document.createElement("INPUT");
-        var submit = document.createElement("INPUT");
+    if (get_info_from_cookie("forbidden") === "") {
+        if (document.getElementById("form-container") === null) {
+            var container = document.createElement("SECTION");
+            var form = document.createElement("FORM");
+            var title = document.createElement("SECTION");
+            var input = document.createElement("SECTION");
+            var login = document.createElement("INPUT");
+            var password = document.createElement("INPUT");
+            var submit = document.createElement("INPUT");
 
-        container.id = "form-container";
-        form.onsubmit = function () {
-            return connexion();
-        };
-        title.className = "form-title";
-        title.innerHTML = "Entrez vos identifiants";
-        input.id = "form-input";
-        login.id = "login-input";
-        login.className = "input-text";
-        login.setAttribute("type", "text");
-        login.setAttribute("placeholder", "Login");
-        password.id = "password-input";
-        password.className = "input-text";
-        password.setAttribute("type", "password");
-        password.setAttribute("placeholder", "password");
-        submit.className = "input-submit";
-        submit.setAttribute("type", "submit");
-        submit.value = "Connect";
+            container.id = "form-container";
+            form.onsubmit = function () {
+                return connexion();
+            };
+            title.className = "form-title";
+            title.innerHTML = "Entrez vos identifiants";
+            input.id = "form-input";
+            login.id = "login-input";
+            login.className = "input-text";
+            login.setAttribute("type", "text");
+            login.setAttribute("placeholder", "Login");
+            password.id = "password-input";
+            password.className = "input-text";
+            password.setAttribute("type", "password");
+            password.setAttribute("placeholder", "password");
+            submit.className = "input-submit";
+            submit.setAttribute("type", "submit");
+            submit.value = "Connect";
 
-        document.body.appendChild(container);
-        container.appendChild(form);
-        form.appendChild(title);
-        form.appendChild(input);
-        form.appendChild(submit);
-        input.appendChild(login);
-        input.appendChild(password);
+            document.body.appendChild(container);
+            container.appendChild(form);
+            form.appendChild(title);
+            form.appendChild(input);
+            form.appendChild(submit);
+            input.appendChild(login);
+            input.appendChild(password);
+        }
     }
 }
 
